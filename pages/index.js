75210fd2f99297eb8315c010e796f10f1915949f
kangminhyuk1111 <rkdglqkr12@naver.com>
2022-09-25 21:20:40 +0900

    20220925-ktx

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { useEffect , useState } from 'react';
 
 export default function Home() {
+  const inputState = useState({
+
+  })
   useEffect(() => {
     async function getData(){
       const apiGetData = 'https://localhost:3000/api/getdata';
-      const response = await fetch(apiGetData);
-      const res = await response.json();
+      const response = null;
+      const res = null;
       console.log(res);
     }
     getData();
   }, [])
-  
+  const inputData = (e) =>{
+    
+  }  
   return (
     <div className='App'>
       hello world!
@@ -21,6 +26,11 @@ export default function Home() {
       <Link href='/sub/about'>hi2</Link>
       <br/>
       <Link href='/sub/1/rkdglqkr12'>hi3</Link>
+      <form>
+        <input type='text' placeholder='id' name='id' onChange={inputData}/>
+        <input type='password' placeholder='pw' name='pw' onChange={inputData}/>
+        <input type='submit' onClick={''}/>
+      </form>
     </div>
   )
 }
